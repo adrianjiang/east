0380a91c297cde87c99aff1a3d74d12913dae377
adrianjiang <13997588305@163.com>
2017-03-13 23:20:52 +0800

    文件打包最简结构

---
+++ src/index.js
@@ -0,0 +1,79 @@
+/*
+这个文件应该是入口文件
+
+*/
+
+
+require('./A-east/east-base.js');
+require('./A-east/east-base.less');
+
+
+
+
+
+//east-pc-debug.js
+function package_pc_debug(){
+
+}
+
+
+//east-mobile-debug.js
+function package_mobile_debug(){
+	
+}
+
+
+//east-pc.js
+function package_pc(){
+	
+}
+
+
+//east-mobile.js
+function package_mobile(){
+	
+}
+
+
+
+
+
+
+
+// import 'core-js/fn/object/assign';
+// import ReactDOM from 'react-dom';
+// import App from './components/Main';
+
+// require('./components/Main');
+
+// require('./style/base.less');
+// require('./style/base.sass');
+// import './style/base.sass'
+
+// require('./style/style.js');
+
+// require('./A-east/east.js');//这个框架的定义
+// require('./control/control');
+// require('./VComponent/vc');
+
+// require('./view/view');
+
+
+
+
+
+// import Atest from './components/Areact';
+
+// console.log('index.js执行了');
+
+// Render the main component into the dom
+
+
+
+
+
+// A.set('PATH_PLUGIN','http://skssapp.7east.cn/api!');
+
+// A.set('API','http://skssapp.7east.cn/api!');
+// A.set('URL_API','http://skssapp.7east.cn/api!');
+// A.set('URL_RN','skssapp.7east.cn');
